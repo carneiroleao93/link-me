45c8b8737135b589885d41fee495446efc73ab84
Luis Carneiro Leão <cria.carneiroleao@gmail.com>
2020-01-22 22:09:40 -0300

    Integrando o JS com Firebase

---
+++ js/init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDzyReOCPfag-vOISjFYOCKklN6g6HbRAY",
+    authDomain: "linkme-dde78.firebaseapp.com",
+    databaseURL: "https://linkme-dde78.firebaseio.com",
+    projectId: "linkme-dde78",
+    storageBucket: "linkme-dde78.appspot.com",
+    messagingSenderId: "308905916088",
+    appId: "1:308905916088:web:ab96cfb4f1c3101499264d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
